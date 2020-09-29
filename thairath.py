20de27e2020cfa69ec1f3002b9edfc0911f69b6a
nakarinh14 <nakarinh14@gmail.com>
2020-09-30 00:58:09 +0700

    Working parser for Thairath XML Sitemap

--- thairath.py
+++ thairath.py
@@ -4,9 +4,15 @@ from lxml import etree
 url = "https://www.thairath.co.th/sitemap-daily.xml"
 res = requests.get(url)
 tree = etree.XML(res.content)
+NS = {'s': "http://www.sitemaps.org/schemas/sitemap/0.9", 'image':"http://www.google.com/schemas/sitemap-image/1.1"}
 
-NS = {'s': "http://www.sitemaps.org/schemas/sitemap/0.9"}
-loc_list = tree.xpath("//s:url/s:loc", namespaces=NS)
+# for a in root.iterfind(f".//{{{NS['s']}}}loc"):
+    # print(a.text)
+parsed_elements = tree.xpath("//s:url/s:loc | //image:image/image:loc | //image:image/image:title | //image:image/image:caption", namespaces=NS)
 
-for loc in loc_list:
-    print(loc.text)
+for i in range(int(len(parsed_elements)/4)):
+    news_url, img_url, title, caption = parsed_elements[(i*4)].text, parsed_elements[(i*4)+1].text, parsed_elements[(i*4)+2].text, parsed_elements[(i*4)+3].text
+    print(f"News url {news_url}")
+    print(f"img url {img_url}")
+    print(f"title url {title}")
+    print(f"caption url {caption}")
