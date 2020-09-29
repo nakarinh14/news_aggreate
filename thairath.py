2bbeeef0c03bfa333e5033c85ab35b09e4d3d502
nakarinh14 <nakarinh14@gmail.com>
2020-09-29 21:40:56 +0700

    Update

--- thairath.py
+++ thairath.py
@@ -2,8 +2,8 @@ import requests
 from lxml import etree
 
 url = "https://www.thairath.co.th/sitemap-daily.xml"
-res = request.get(url)
-tree = etree.XML(response.content)
+res = requests.get(url)
+tree = etree.XML(res.content)
 
 NS = {'s': "http://www.sitemaps.org/schemas/sitemap/0.9"}
 loc_list = tree.xpath("//s:url/s:loc", namespaces=NS)
