e6cbec2784f97de7e6d18e4bae87cf5538b78de1
nakarinh14 <nakarinh14@gmail.com>
2020-10-10 17:29:31 +0700

    Add API endpoint for user registration.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,30 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+const db = require('../db/db')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/', (req, res) => {
+    console.log(req.body)
+    db.findUser([req.body.username], (err, user) => {
+        if(!user.rows.length){
+            bcrypt.hash(req.body.password, 5, (err, hash) => {
+                if(err) console.log(err)
+                else{
+                    db.addUser([req.body.username, hash], (err) => {
+                        if(err)  res.json({status: 'failed', message: 'Add failed'})
+                        else res.json({status: 'success', message: 'User added successfully'})
+                    })
+                }
+            })
+        } else {
+            res.json({status: 'failed', message: 'User already exist'});
+        }
+    })
+
+})
+
+// router.put('/setting', (req, res, next) => {
+//     db.updateSetting()
+// })
 
 module.exports = router;
