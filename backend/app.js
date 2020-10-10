193fbc428d016f24948c16632655998bbe026276
nakarinh14 <nakarinh14@gmail.com>
2020-10-10 17:30:09 +0700

    Update app.js and .env

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,16 @@
+require('dotenv').config()
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const passport = require('passport')
+const session = require('express-session');
+require('./passport-config')(passport);
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-
+const authRouter = require('./routes/auth');
+const newsRouter = require('./routes/news');
 const app = express();
 
 app.use(logger('dev'));
@@ -13,8 +18,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true
+}))
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth',authRouter);
+app.use('/api/news', newsRouter);
 
 module.exports = app;
