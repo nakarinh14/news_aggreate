9337aeaa973032b1af082b6966950d1d99a2324c
nakarinh14 <nakarinh14@gmail.com>
2020-11-25 11:11:34 +0700

    Add 400 and 500 status code on failed registration.

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,7 +7,7 @@ addNewUser = async (username, password) => {
     try{
         const hash = await bcrypt.hash(password, 5)
         await db.addUser([username, hash]);
-        await db.addUserSettings([username]);
+        // await db.addUserSettings([username]);
     }
     catch(err){
         console.log(err)
@@ -15,14 +15,13 @@ addNewUser = async (username, password) => {
 }
 
 router.post('/register', (req, res) => {
-    console.log(req.body)
     db.findUser([req.body.username], (err, user) => {
         if(!user.rows.length){
             addNewUser(req.body.username, req.body.password)
-                .then(() => res.json({status: 'success', message: 'User added successfully'}))
-                .catch(() => res.json({status: 'failed', message: 'Error adding user'}))
+                .then(() => res.json({ message: 'User added successfully'}))
+                .catch(() => res.status(500).json({message:'Error adding user'}))
         } else {
-            res.json({status: 'failed', message: 'User already exist'});
+            res.status(400).json({message: 'User already exist'});
         }
     })
 
